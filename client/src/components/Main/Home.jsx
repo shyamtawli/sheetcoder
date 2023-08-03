14ae5f7617643f4e527e1dd4fe2fa51d9f8818e0
shyamtawli <shyamtawli2@gmail.com>
2023-08-03 17:59:09 +0530

    Table seperated

--- client/src/components/Main/Home.jsx
+++ client/src/components/Main/Home.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Navbar from "../Topbar/Navbar";
 import "./Home.css";
-import { Link } from "react-router-dom";
 
 import TableData from "./TableData";
 
@@ -25,45 +24,7 @@ function Home() {
   return (
     <>
       <Navbar />
-      <div className="main-container">
-        <table className="problem-table">
-          <thead className="table-head-col">
-            <tr>
-              <th className="table-heading">Rank</th>
-              <th className="table-heading">Title</th>
-              <th className="table-heading">Difficulty</th>
-              <th className="table-heading">Category</th>
-            </tr>
-          </thead>
-          {problems.length > 0 ? (
-            <tbody>
-              {problems.map((doc, idx) => {
-                const difficulyColor =
-                  doc.difficult === "Easy"
-                    ? "green"
-                    : doc.difficult === "Medium"
-                    ? "yellow"
-                    : "red";
-                return (
-                  <tr
-                    className={`${idx % 2 == 1 ? "row-odd" : ""}`}
-                    key={doc.id}
-                  >
-                    <th>{doc.order}</th>
-                    <td className="title">
-                      <Link to={`/problems/${doc.id}`}>{doc.title}</Link>
-                    </td>
-                    <td style={{ color: difficulyColor }}>{doc.difficult}</td>
-                    <td>{doc.category}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          ) : (
-            <p>Loading...</p>
-          )}
-        </table>
-      </div>
+      <TableData problems={problems} />
     </>
   );
 }
--- client/src/components/Main/TableData.jsx
+++ client/src/components/Main/TableData.jsx
@@ -1,8 +1,41 @@
-// import problems from "../../problem.js";
+import { Link } from "react-router-dom";
 
-const TableData = (problems) => {
-  console.log(problems);
-  return <div></div>;
+const TableData = ({ problems }) => {
+  // console.log(problems);
+  return (
+    <div className="main-container">
+      <table className="problem-table">
+        <thead className="table-head-col">
+          <tr>
+            <th className="table-heading">Rank</th>
+            <th className="table-heading">Title</th>
+            <th className="table-heading">Difficulty</th>
+            <th className="table-heading">Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {problems.map((doc, idx) => {
+            const difficulyColor =
+              doc.difficult === "Easy"
+                ? "green"
+                : doc.difficult === "Medium"
+                ? "yellow"
+                : "red";
+            return (
+              <tr className={`${idx % 2 == 1 ? "row-odd" : ""}`} key={doc.id}>
+                <th>{doc.order}</th>
+                <td className="title">
+                  <Link to={`/problems/${doc.id}`}>{doc.title}</Link>
+                </td>
+                <td style={{ color: difficulyColor }}>{doc.difficult}</td>
+                <td>{doc.category}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default TableData;
