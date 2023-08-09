c38a71ddae33a7b863ff742d8937a972988770c5
shyamtawli <shyamtawli2@gmail.com>
2023-08-09 17:52:22 +0530

    react-split

--- client/src/components/Workspace/ProblemDescription.jsx
+++ client/src/components/Workspace/ProblemDescription.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Workspace.css";
 
 function ProblemDescription() {
-  return <div className="problem-desc">ProblemDescription</div>;
+  return <div className="problem-desc-container">Problem</div>;
 }
 
 export default ProblemDescription;
