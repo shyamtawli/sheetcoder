9953ea9acf58833987571db02a04b04de9bf77c1
shyamtawli <shyamtawli2@gmail.com>
2023-08-29 19:45:52 +0530

    Error FIX

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://sheetcoder.vercel.app/"],
+    origin: ["https://sheetcoder.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
   })
